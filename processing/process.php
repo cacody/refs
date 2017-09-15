e0a9eef2baf07187eebc20ae1516348334ea0a01
Corin Cody <cacody@umich.edu>
2017-09-15 11:21:57 -0400

    turning on error reporting

--- processing/process.php
+++ processing/process.php
@@ -2,6 +2,9 @@
 
 //date_default_timezone_set('America/Detroit');
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 $docRoot = $_SERVER['DOCUMENT_ROOT'];
 
